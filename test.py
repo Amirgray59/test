d24a2ea7f83336a2c94cf639838f41bc924ceb0a
Amirgray59 <amiroper59@gmail.com>
2024-07-26 17:30:13 +0330

    v.0.2

--- test.py
+++ test.py
@@ -7,4 +7,6 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
+model = LinearRegression()
+df = sns.load_dataset("iris")
 
